f291989da65bc4c7a494da165a4bf557435f8c7e
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2020-12-22 23:19:20 -0800

    generate type files and implement google login flow

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,66 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache
-  } from '@apollo/client';
-import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Home, Author, Review, Reviews, NotFound, User } from './sections'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+import {
+	Home,
+	Author,
+	Review,
+	Reviews,
+	NotFound,
+	User,
+	Login,
+} from './sections';
+import { Viewer } from './lib/types';
 import reportWebVitals from './reportWebVitals';
-import './styles/index.css'
+import './styles/index.css';
 
 const client = new ApolloClient({ uri: '/api', cache: new InMemoryCache() });
 
+const initialViewer: Viewer = {
+	id: null,
+	token: null,
+	avatar: null,
+	didRequest: false,
+};
+
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/author" component={Author} />
-        <Route exact path="/review/:id" component={Review} />
-        <Route exact path="/reviews/:location?" component={Reviews} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  )
-}
+	const [viewer, setViewer] = useState<Viewer>(initialViewer);
+	console.log('viewer :>> ', viewer);
+
+	return (
+		<Router>
+			<Layout id='app'>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/author' component={Author} />
+					<Route exact path='/review/:id' component={Review} />
+					<Route
+						exact
+						path='/reviews/:location?'
+						component={Reviews}
+					/>
+					<Route exact path='/user/:id' component={User} />
+					<Route
+						exact
+						path='/login'
+						render={(props) => (
+							<Login {...props} setViewer={setViewer} />
+						)}
+					/>
+					<Route component={NotFound} />
+				</Switch>
+			</Layout>
+		</Router>
+	);
+};
 
 render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
